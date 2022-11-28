b59b30f83dfb0e470e71117ffd8c5ce584d5061c
TMV <m.timarsuev@gmail.com>
2022-11-28 17:14:35 +0300

    Ethereal message test

--- controllers/email.js
+++ controllers/email.js
@@ -1,5 +1,32 @@
+const nodemailer = require(`nodemailer`);
+
 const send = async (req, res, next) => {
-  return res.status(200).send(`EMAIL SENT`);
+  const { name, email } = req.user;
+
+  const testAccount = await nodemailer.createTestAccount();
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: testAccount.user,
+      pass: testAccount.pass,
+    },
+  });
+
+  const info = await transporter.sendMail({
+    from: '"EmailAPI" <foo@bar.com>',
+    to: `${email}`,
+    subject: `Hello, ${name}✔`,
+    text: `Hello, ${name}! This is a plain text of the message`,
+    html: `<h2>Hello, ${name}!</h2><p>This is an HTML message</p>`,
+  });
+
+  console.log("Message sent: %s", info.messageId);
+
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+  res.json(info);
 };
 
 module.exports = send;
--- middleware/notFound.js
+++ middleware/notFound.js
@@ -1,8 +1,6 @@
 const { StatusCodes } = require(`http-status-codes`);
 
 const notFound = (req, res, next) => {
-  console.log(`!!!!!!!NOT FOUND`);
-
   return res
     .status(StatusCodes.NOT_FOUND)
     .send(`Route not found, try another one!`);
